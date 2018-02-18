b6fe27ad1429a6d025329bd082d9d5f37f643e81
Jonathan Marsh <j.r.marsh@ntlworld.com>
2018-02-18 02:49:49 +0000

    Horizontal + info added

--- index.js
+++
@@ -1,9 +0,0 @@
-(function (){
-	var items = document.getElementsByClassName("dependencies");
-	for(var index = 1; index < items.length; index++){
-		var difference = items[index].parentElement.parentElement.offsetWidth - items[index].offsetWidth
-		if(difference > 0){
-			console.log(document.getElementsByClassName("dependencies")[index].previousSibling.innerHTML);
-		}
-	}
-})();
