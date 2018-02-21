6fb1318f6ddd02f20830bd22ed6004c8d389ba7b
Jonathan Marsh <j.r.marsh@ntlworld.com>
2018-02-21 11:50:37 +0000

    Generation nearly done

--- generate.js
+++ generate.js
@@ -1,30 +1,45 @@
-var dependencies = [], tree = {}, indexList = [];
-function findInTree(){
+var dependencies = [], whitelisted = [], tree = {warnings: 0, conflicts:0, indexes:[], children:[]}, indexList = [];
+function createDependency(dependency, mode){
+	var generated = '<label class="dependency">\n';
+	if(dependency.children.length > 0) generated += '<input type="radio" name="tier'+dependency.depth+'"/>\n';
+	generated += '<div class="item">\n<div class="icons">\n';
+	if(dependency.warnings > 0) generated += '<div class="warnings">'+dependency.warnings+'</div>\n';
+	if(dependency.conflicts > 0) generated += '<div class="conflicts">'+dependency.conflicts+'</div>\n';
+	generated += '</div>\n<div class="content '+dependency.flag+'">\n';
+	generated += '<p>'+dependency.name+'</p>\n';
+	if(mode != 2) generated += '<a href="#" onclick="show('+dependency.indexes+')">More info...</a>\n';
+	generated += '</div>\n</div>\n';
+	if(dependency.children.length > 0){
+		generated += '<div class="dependencies">\n';
+		for(var index = 0; index < dependency.children.length; index++)
+			generated+=createDependency(dependency.children[index], mode);
+		generated+='</div>\n';
+	}
+	generated+='</label>\n';
+	return generated;
+}
+function generate(mode, name){
+	var generated = createDependency(tree, mode);
+	console.log(mode==0);
+	console.log(mode===0);
+	switch(mode){
+		case "0": 
+		console.log("test");
+			generated = allinone(generated);
+			console.log("test");
+			console.log(generated);
+			break;
+		case "1": 
+			generated = separate(generated);
+			console.log(generated);
+			break;
+		case "2": 
+			generated = htmlcss(generated);
+			console.log(generated);
+			break;
+	}
 	
-}
-function createDependency(dependency){
-	var generated =
-	`<label class="dependency">
-		<input type="radio" name="tier${depth}"/>
-		<div class="item">
-			<div class="icons">
-				<div class="warnings">${warnings}</div>
-				<div class="conflicts">${conflicts}</div>
-			</div>
-			<div class="content ${flag}">
-				<p>${name}</p>
-				<a href="#" onclick="show(${indexes})">More info...</a>
-			</div>
-		</div>
-		<div class="dependencies">`;
-	for()
-		generated+=createDependency();
-	return generated+=`</div></label>`;
-}
-function generate(combine){
-	var dependencies = "";
-	var generated = (combine)?allinone(dependencies):separate(dependencies);
-	download(generated, 'generated.html','html');
+	download(generated, name+'.html','html');
 }
 function download(data, filename, type) {
     var file = new Blob([data], {type: type});
@@ -43,43 +58,50 @@ function download(data, filename, type) {
         }, 0); 
     }
 }
-function safeLicense(){
-	return true;
+function safeLicense(license){
+	if(whitelisted.length == 0)
+		return true;
+	for(var index = 0; index < whitelisted.length; index++)
+		if(whitelisted[index] == license)
+			return true;
+	return false;
 }
 function processTree(file){
 	var lines = file.content.split('\n'),
 		lines = lines.filter((line)=>line.length>0);
 	var indexes = [], prevDepth = 0;
-	for(var index = 0; index < lines.length; index++){
-		var depth = (/[^\w]*/.exec(lines[index])[0].length)/3,
-			cut = /[^\w]*/.exec(lines[index])[0],
-			vGAV = lines[index].replace(cut,''),
-			GAV = /[^:]*:[^:]*/.exec(vGAV),
-			info = -1;
+	for(var lineIndex = 0; lineIndex < lines.length; lineIndex++){
+		var depth = (/[^\w]*/.exec(lines[lineIndex])[0].length)/3,
+			GAV = /[^:]*:[^:]*/.exec(lines[lineIndex].replace(/[^\w]*/.exec(lines[lineIndex])[0],'')),
+			info = -1,
+			flag = "";
 		
 		for(var index = 0; index < dependencies.length; index++)
 			if(dependencies[index].GAV == GAV)
 				info = dependencies[index];
 		
+		if(info == -1) flag = "missing";
+		else if(!safeLicense(info.license)) flag = "conflict";
+		
+		console.log(indexes);
 		var currentItem = tree;
-		for(var index = 0; index < indexes.length-1; index++){
-			if (info == -1)
-				currentItem["warnings"]++; 
+		for(var index = 0; index < indexes.length - 1; index++){
+			if (flag == "missing") currentItem["warnings"]++; 
+			if (flag == "conflict") currentItem["conflicts"]++; 
 			currentItem = currentItem.children[indexes[index]];
 		}
-		
-		flag = 0;
-		if(info == -1) flag = 1;
-		if(!safeLicense) flag = 2;
-		
-		currentItem.children.push({warnings: 0, conflicts:0, flag:flag, info:info, indexes: indexes, children:[]});
+		if (flag == "missing") currentItem["warnings"]++; 
+		if (flag == "conflict") currentItem["conflicts"]++; 		
+		currentItem.children.push({warnings: 0, conflicts:0, flag:flag, license:info.license, name:info.name, GAV:info.GAV, depth: depth, children:[]});
 		
 		for(var popNamount = 0; popNamount < prevDepth-depth; popNamount++)
 			indexes.pop();
 		indexes[depth]++;
 		if(!indexes[depth]) indexes[depth] = 0;
+		
 		prevDepth = depth;
 	}
+	console.log("INFO: Tree structure");
 	console.log(tree);
 }
 function process3rdParty(file){
@@ -98,16 +120,27 @@ function process3rdParty(file){
 			dependencies.push({license: components[1], name:calculatedName.trim(), GAV:components[components.length-2] });
 		}
 	}
+	console.log("INFO: Module dependency information");
 	console.log(dependencies);	
 }
-function process(files,combine){
-	for(var index = 0; index < files.length; index++){
-		if(files[index].name == "THIRD-PARTY.txt")
+function process(files,mode, name){
+	var count=0, required=2;
+	for(var index = 0; index < files.length; index++)
+		if(files[index].name == "THIRD-PARTY.txt"){
 			process3rdParty(files[index]);
-		if(files[index].name == "tree.txt")
+			count++;
+			required--;
+		}else if(files[index].name == "tree.txt"){
 			processTree(files[index]);
-		generate(combine);
-	}
+			count++;
+			required--;
+		}else {
+			
+		}
+	if (count < files.length)
+		console.log("WARN: Not all files were processed, ensure they have exactly the correct file names");
+	if(required==0) generate(mode, name);
+	else console.log("CRIT: Missing required file");
 }
 function read(e){
 	e.preventDefault();
@@ -115,7 +148,15 @@ function read(e){
 		return;
 	var files = [],
 		group =  document.getElementById('files').files,
-		combine = document.getElementById('combined').checked;
+		name =  document.getElementById('name').value || "generated",
+		combine = document.getElementsByName('generation'),
+		mode = 0;
+	for(var i = 0; i < combine.length; i++)
+		if(combine[i].checked){
+			mode = combine[i].value;
+			break;
+		}
+		
 	document.getElementsByTagName('body')[0].innerHTML = "";
 	for(var index = 0; index < group.length; index++){
 		var reader = new FileReader();
@@ -123,7 +164,7 @@ function read(e){
 		reader.onload = function(e2) {
 			files.push({name:this.name,content:e2.currentTarget.result});
 			if(files.length >= 2)
-				process(files,combine);
+				process(files,mode,name);
 		};
 		reader.readAsText(group[index]);
 	}
--- template.js
+++ template.js
@@ -96,9 +96,10 @@ function allinone(dependencies){return `
 			.item:not(:only-child):hover,
 			input[type="radio"]:checked + .item {
 				background: #ddd;
+				cursor:pointer;
 			}
 			/*white up ones with sub modules*/
-			input + .item .content{	
+			input + .item .content:not(.missing):not(.conflict){	
 				background:#fff;
 				border-bottom:.2rem solid #eaeaea;
 			}
@@ -126,14 +127,14 @@ function allinone(dependencies){return `
 				cursor:pointer;
 			}
 			/*Highlight conflicting licenses*/
-			.conflict{
+			.content.conflict{
 				background:red;
 				border-bottom-color:#c80000;
 				color:white;
 			}
 			
 			/*Highlight missing licenses*/
-			.missing{
+			.content.missing{
 				background:#ffbc00;
 				border-bottom-color:#dfa503;
 				color:white;
@@ -228,6 +229,177 @@ function allinone(dependencies){return `
 			alert(ignore);
 		}
 		</script>
+	</body>
+</html>
+`;}
+function htmlcss(dependencies){return `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="Dependency License Explorer">
+		<meta name="keywords" content="HTML,CSS,JavaScript,License,Dependency">
+		<meta name="author" content="Jonathan Marsh, Charlie Parker">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Dependency License Explorer</title>
+		<style>
+		body {
+			margin:1px;
+			background:#eee;
+			font-family:Arial;
+			height:min-content;
+		}
+		/*List content vertically,*/
+		.dependencies {
+			display: inline-flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			width: min-content;
+			position: absolute;
+			left: 100%;
+			top: 0;
+			height:100%;
+		}
+		/*giving all the same width & height*/
+		.dependency{
+			height: 6rem;
+		}
+		/*We don't want to start 100vw to the left*/
+		body > .dependencies{
+			left:0;
+		}
+		/*Hide the radio inputs*/
+		input[type="radio"]{
+			display:none;
+			position:absolute;
+		}
+		/*Hide the irrelevant tiers*/
+		input[type="radio"]:not(:checked) ~ .dependencies{
+			display:none;
+		}
+		/*Content styling*/
+		.content{
+			position:relative;
+			z-index:2;
+			background:#aaa;
+			padding:1.2rem 2rem .5rem 2rem;
+			border-bottom:.2rem solid #999;
+			border-radius:.3rem;
+			white-space:nowrap;
+			text-align:center;
+			text-shadow: -1px -1px 2px rgba(0,0,0,0.2);
+		}
+			/*Highlight directory*/
+			.item:not(:only-child):hover,
+			input[type="radio"]:checked + .item {
+				background: #ddd;
+				cursor:pointer;
+			}
+			/*white up ones with sub modules*/
+			input + .item .content:not(.missing):not(.conflict){	
+				background:#fff;
+				border-bottom:.2rem solid #eaeaea;
+			}
+			/*Stylise text*/
+			.content * {
+				overflow: hidden;
+				white-space: nowrap;
+				text-overflow: ellipsis;
+				max-width:10rem;
+				margin:0;
+			}
+			.content *:nth-child(1){
+				font-size:1rem;
+				font-weight:bold;
+			}
+			/*Highlight conflicting licenses*/
+			.conflict{
+				background:red;
+				border-bottom-color:#c80000;
+				color:white;
+			}
+			
+			/*Highlight missing licenses*/
+			.missing{
+				background:#ffbc00;
+				border-bottom-color:#dfa503;
+				color:white;
+			}
+		/*Add horizontal*/
+		.item{
+			position:relative;
+			padding:1rem 2rem;
+			height:4rem;
+		}
+		.item::before,
+		input[type="radio"]:checked + .item::after {
+			content: " ";
+			display: block;
+			height: 2px;
+			position: absolute;
+			top: 50%;
+			left:0;
+			width: 2rem;
+			background: #aaa;
+		}
+		input[type="radio"]:checked + .item::before {
+			width:100%;
+		}
+		/*Add vertical*/
+		input[type="radio"]:checked + .item::after {
+			right: -2px;
+			width: 2px;
+			left: unset;
+			top: unset;
+			margin-bottom:-2px;
+			height: 100vh;
+			bottom: 50%;
+			z-index:2;
+		}
+		.dependency:not(:last-child)::before{
+			content: " ";
+			display: block;
+			height: 6rem;
+			position: absolute;
+			left: 0;
+			margin-top: 3rem;
+			z-index: 2;
+			width: 2px;
+			background: #aaa;
+		}
+		/*icons*/
+		.icons{
+			position:absolute;
+			z-index:3;
+			right:2.1rem;
+		}
+		.icons *{display:inline;}
+		/*.warnings*/
+		.warnings::after {
+			content:" ";
+			background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 110"> <path stroke-linecap="round" stroke-linejoin="round" stroke="black" stroke-width="5" fill="%23ffd42a" d="M 60,10 L 10,100 L 110,100 Z" /><g text-anchor="middle" dominant-baseline="middle" font-family="Sans-serif" font-size="60" font-weight="bolder"><text x="60" y="68" fill="%23000">!</text></g></svg>');
+			display: inline-block;
+			width:1rem;
+			height:1rem;
+			vertical-align:text-bottom;
+			background-repeat:no-repeat;
+		}
+		/*.conflicts*/
+		.conflicts::after{
+			content:" ";
+			background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><circle cx="50" cy="54" r="40" fill="%23c0392b"/><circle cx="50" cy="50" r="40" fill="%23e74c3c"/><g text-anchor="middle" dominant-baseline="middle" font-family="Verdana" font-size="50" font-weight="bolder"><text x="50" y="55"  fill="%23c0392b">X</text><text x="50" y="52" fill="%23fff">X</text></g></svg>');
+			display: inline-block;
+			width:1rem;
+			height:1rem;
+			vertical-align:text-bottom;
+			background-repeat:no-repeat;
+		}
+		</style>
+	</head>
+	<body>
+		<div class="dependencies">
+			${dependencies}
+		</div>
 	</body>
 </html>
 `;}
